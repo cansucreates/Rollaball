72e345e26d9422a9a9ace56baf3be6930a88d1c4
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2025-02-01 21:51:38 +0300

    Add camera follow functionality

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset; // The distance between the player and the camera
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        offset = transform.position - player.transform.position; // camera position - player position
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate() // LateUpdate is called after all Update functions have been called
     {
-        
+        transform.position = player.transform.position + offset; // camera position = player position + offset
     }
 }
